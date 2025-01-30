4180cbf741f7f9474f6a3c8e9bc16d3b31527d8c
Jeroen Smink <jeroensmink98@gmail.com>
2025-01-30 13:42:14 +0100

    ci: upgrade to net9
    Changelog: ci

--- test.sh
+++ test.sh
@@ -1,31 +1,19 @@
 
 # tar module
-tar -czvf --exclude='.DS_Store' -f my_module.tgz my_module || true
+
 
 #!/bin/bash
 API_KEY="Cd16694YKkKxvjJC1rzCfe0T/fmQLTajDND3vQ2ic1I="
 NAMESPACE="acme"
 MODULE_NAME="something"
 VERSION="2.0.1"
+PORT=3000
+DOMAIN="modules.devopsfrontier.com"
 
 curl -X 'POST' \
-  "https://localhost:443/v1/module/$NAMESPACE/$MODULE_NAME/$VERSION" \
+  "http://$DOMAIN:$PORT/v1/module/$NAMESPACE/$MODULE_NAME/$VERSION" \
   -H 'accept: */*' \
   -H 'Content-Type: multipart/form-data' \
   -H "X-API-Key: $API_KEY" \
   -F 'file=@my_module.tgz;type=application/x-gzip'
 
-
-curl -X 'POST' \
-  "https://localhost:443/v1/module/new/$MODULE_NAME/$VERSION" \
-  -H 'accept: */*' \
-  -H 'Content-Type: multipart/form-data' \
-  -H "X-API-Key: $API_KEY" \
-  -F 'file=@my_module.tgz;type=application/x-gzip'
-
-sleep 2
-
-curl -X 'DELETE' \
-  "https://localhost:443/v1/module/$NAMESPACE/$MODULE_NAME/$VERSION" \
-  -H 'accept: */*' \
-  -H "X-API-Key: $API_KEY"
