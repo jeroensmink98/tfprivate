4fdd198dfc0c90d890da7f1e2d9b7971d7886aca
Jeroen Smink <jeroensmink98@gmail.com>
2025-01-30 10:56:24 +0100

    chore: removed env files
    Changelog: chore

--- run-container.sh
+++ run-container.sh
@@ -8,22 +8,24 @@ APP_INSIGHT_KEY=""
 # Build the container if it doesn't exist
 docker build -t tfprivate-api ./tfprivate.Api
 
-# Remove existing container if it exists
-docker rm -f tfprivate-api 2>/dev/null || true
 
-# Run the container
-docker run -d \
-  --name tfprivate-api \
-  --restart unless-stopped \
-  -p 445:443 \
-  -p 80:80 \
-  -v ${HOME}/.aspnet/https/:/root/.aspnet/https/ \
-  -e STORAGE_ACCOUNTNAME="$STORAGE_ACCOUNTNAME" \
-  -e STORAGE_ACCESS_KEY="$STORAGE_ACCESS_KEY" \
-  -e API_KEY="$API_KEY" \
-  ${APP_INSIGHT_KEY:+-e APP_INSIGHT_KEY="$APP_INSIGHT_KEY"} \
-  -e ASPNETCORE_ENVIRONMENT="Production" \
-  tfprivate-api
 
-# Show the logs
-docker logs -f tfprivate-api 
+# # Remove existing container if it exists
+# docker rm -f tfprivate-api 2>/dev/null || true
+
+# # Run the container
+# docker run -d \
+#   --name tfprivate-api \
+#   --restart unless-stopped \
+#   -p 445:443 \
+#   -p 80:80 \
+#   -v ${HOME}/.aspnet/https/:/root/.aspnet/https/ \
+#   -e STORAGE_ACCOUNTNAME="$STORAGE_ACCOUNTNAME" \
+#   -e STORAGE_ACCESS_KEY="$STORAGE_ACCESS_KEY" \
+#   -e API_KEY="$API_KEY" \
+#   ${APP_INSIGHT_KEY:+-e APP_INSIGHT_KEY="$APP_INSIGHT_KEY"} \
+#   -e ASPNETCORE_ENVIRONMENT="Production" \
+#   tfprivate-api
+
+# # Show the logs
+# docker logs -f tfprivate-api 
